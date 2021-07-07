91a8c8dd822e3d1ed0a473fe75c7fe1c6b087e37
Ryuya Asato <e21001@std.it-college.ac.jp>
2021-07-07 17:44:42 +0900

    if構文

--- index.php
+++ index.php
@@ -8,4 +8,31 @@
   $hello = null;
   $msg = $hello ?? 'Hello';
   echo $msg;
+
+  $score = 89;
+  $messageg = '';
+  if ($score >= 90) {
+    $message = 'すごい！';
+  } else {
+    $message = 'くたばれ！';
+  }
+  echo $message;
+
+  // 条件分岐による空欄チェック処理
+
+  // 論理演算子で否定
+  if (!$a) {
+    // ここに処理
+  }
+
+  // 緩やかな比較演算子の使用
+  if ($a == '') {
+    // ここに処理
+  }
+
+  // empty関数を使用
+  if (empty($a)) {
+    // ここに処理
+  }
+  
 ?>
