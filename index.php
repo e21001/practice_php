35b10eb6b09f4cd1e2ed2cd994b9cd845c50dd7a
Ryuya Asato <e21001@std.it-college.ac.jp>
2021-07-08 18:34:20 +0900

    break命令とcontinue命令

--- index.php
+++ index.php
@@ -131,4 +131,22 @@
     $num += 30;
   }
   echo '$numが200を超えたためループを抜けました。';
+
+  // break命令、continue命令
+  $total = 0;
+  $numbers = [10, 2, -5, 3, 'abc', 6, 1];
+  echo '正の整数を対象に配列の要素を足し算します。', PHP_EOL;
+  foreach ($numbers as $number) {
+    if (!is_numeric($number)) {
+      echo "数値ではない値を検知したため計算を中断します。(対象値：{$number})", PHP_EOL;
+      break;
+    }
+    if ($number < 0) {
+      echo "マイナス値は計算しません。(対象値：{$number})", PHP_EOL;
+      continue;
+    }
+    $total += $number;
+  }
+  echo "合計：{$total}";
+  
 ?>
