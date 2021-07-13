6c43b93fd45a3f95c318184373f9abb5188036c6
Ryuya Asato <e21001@std.it-college.ac.jp>
2021-07-13 18:06:48 +0900

    useキーワード

--- index2.php
+++ index2.php
@@ -5,4 +5,15 @@ $addFunction = function($a, $b) {
 };
 $total = $addFunction(10, 20);
 echo "計算結果：" . $total;
+
+// useキーワードでクロージャーに変数を引き継ぐ
+$greeting = 'Good';
+
+$greetingMaker = function ($time) use ($greeting) {
+  echo $greeting . ' ' . $time . '<br>';
+};
+
+$greetingMaker('Morning');
+$greetingMaker('Evening');
+
 ?>
