5edb7866071ce8aa1e8c4bb8bcdcef20ef1f8341
Ryuya Asato <e21001@std.it-college.ac.jp>
2021-07-16 14:37:52 +0900

    try~catch~finally構文

--- exception-main.php
+++ exception-main.php
@@ -3,7 +3,14 @@
   <?php
   require_once dirname(__FILE__) . '/exception.php';
   $calculator = new TaxCalculator();
-  $priceWithTax = $calculator->calculate(100, -0.08);
-  echo '計算結果は', $priceWithTax, '円です。';
+  try {
+    $priceWithTax = $calculator->calculate(100, -0.08);
+    echo '計算結果は', $priceWithTax, '円です。';
+  } catch (Exception $exception) {
+    echo '税込金額が計算できませんでした。理由：', $exception->getMessage(), '<br>';
+  } finally {
+    $calculator->reset();
+  }
+  echo 'プログラム処理を終了します。';
   ?>
 </body>
